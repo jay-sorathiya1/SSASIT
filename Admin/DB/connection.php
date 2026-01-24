30470ded1769c3a4e93fad1335b1a019b2aa2de6
jay sorathiya <jaysorathiya911@gmail.com>
2026-01-24 20:58:45 +0530

    Update connection.php

--- Admin/DB/connection.php
+++ Admin/DB/connection.php
@@ -5,6 +5,7 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "ssasit");
 
+
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$conn) {
     die("Connection failed" . mysqli_connect_error());
